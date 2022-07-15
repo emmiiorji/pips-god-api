a80d37281756e8d039f8e725c03db29b2b01edbe
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-15 16:09:13 +0100

    Add data validation methods for transaction model

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,4 @@
 module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
+module.exports.subscriptionValidation = require('./subscription.validation');
+module.exports.transactionValidation = require('./transaction.validation');
---
+++ src/validations/transaction.validation.js
@@ -0,0 +1,34 @@
+const Joi = require('joi');
+const { dateUnits } = require('../config/dateUnits');
+const { transactionStatuses } = require('../config/transactionStatus');
+
+const initializeTransaction = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    subscriptionPlanId: Joi.number().integer().required(),
+  }),
+};
+
+const getTransactions = {
+  query: Joi.object().keys({
+    status: Joi.string().valid(...Object.values(transactionStatuses)),
+    validityUnit: Joi.string().valid(...Object.values(dateUnits)),
+    email: Joi.string().email(),
+    authorizationUrl: Joi.string(),
+    reference: Joi.string(),
+    accessCode: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const verifyTransaction = {
+  reference: Joi.string(),
+};
+
+module.exports = {
+  initializeTransaction,
+  getTransactions,
+  verifyTransaction,
+};
