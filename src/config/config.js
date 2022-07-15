25b6da29ebfe85d52c2aa10b91a260ced9a74f89
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-15 14:25:11 +0100

    Add paystack keys to environment variables config

--- src/config/config.js
+++ src/config/config.js
@@ -23,6 +23,8 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    PAYSTACK_SECRET_KEY: Joi.number().description('the paystack secret key'),
+    PAYSTACK_PUBLIC_KEY: Joi.number().description('the paystack public key'),
   })
   .unknown();
 
@@ -60,5 +62,9 @@ module.exports = {
       },
     },
     from: envVars.EMAIL_FROM,
+  },
+  paystack: {
+    secretKey: envVars.PAYSTACK_SECRET_KEY,
+    publicKey: envVars.PAYSTACK_PUBLIC_KEY,
   },
 };
