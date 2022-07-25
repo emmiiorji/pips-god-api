f9b9d69643e3601beac852a427c0c378f7717455
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-25 11:53:19 +0100

    Create the permission model

---
+++ src/models/permission.model.js
@@ -0,0 +1,20 @@
+const sequelizePaginate = require('sequelize-paginate');
+
+module.exports = (sequelize, dataType) => {
+  const permission = sequelize.define('permission', {
+    name: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+      unique: true,
+    },
+    description: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+  });
+
+  sequelizePaginate.paginate(permission);
+  return permission;
+};
