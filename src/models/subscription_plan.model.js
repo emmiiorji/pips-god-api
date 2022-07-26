0234b4b578ee258ccebaf16e3627a6e464cf7bda
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-26 11:58:47 +0100

    Create the subscription_plan model

---
+++ src/models/subscription_plan.model.js
@@ -0,0 +1,28 @@
+const sequelizePaginate = require('sequelize-paginate');
+const { dateUnits } = require('../config/dateUnits');
+
+module.exports = (sequelize, dataType) => {
+  const subscriptionPlan = sequelize.define('subscription_plan', {
+    name: {
+      type: dataType.STRING,
+      unique: true,
+      allowNull: false,
+      trim: true,
+    },
+    price: {
+      type: dataType.INTEGER,
+      allowNull: false,
+    },
+    validity: {
+      type: dataType.INTEGER,
+      defaultValue: 30,
+    },
+    validityUnit: {
+      type: dataType.ENUM(...Object.values(dateUnits)),
+      defaultValue: dateUnits.DAYS,
+    },
+  });
+
+  sequelizePaginate.paginate(subscriptionPlan);
+  return subscriptionPlan;
+};
