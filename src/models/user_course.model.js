80b5b7dc013fdbcfc3343762aa780934f49afec2
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-26 12:05:59 +0100

    Create the user_course model

---
+++ src/models/user_course.model.js
@@ -0,0 +1,18 @@
+const sequelizePaginate = require('sequelize-paginate');
+
+module.exports = (sequelize, dataType) => {
+  const userCourse = sequelize.define('user_course', {
+    startDate: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    isStarted: {
+      type: dataType.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+
+  sequelizePaginate.paginate(userCourse);
+  return userCourse;
+};
