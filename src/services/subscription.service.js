bd31d9cf4eba34075d5e3b56536cc970524a2635
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-06 16:15:33 +0100

    Add subscription service with method to query subscriptions

---
+++ src/services/subscription.service.js
@@ -0,0 +1,28 @@
+const { db } = require('../models');
+
+/**
+ * Query for subscriptions. Only allowed for admin user
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+const querySubscriptions = async (filter, options) => {
+  // Todo
+  // Change options.sortBy of format: sortField:(desc|asc) to options.order of format: [['sortField', 'DESC'|'ASC']]
+  const subscriptions = await db.subscriptions.paginate({
+    where: filter,
+    ...options,
+    include: {
+      model: db.transactions,
+      attributes: ['id', 'status', 'email', 'reference', 'amount', 'currency'], // only return listed attributes
+    },
+  });
+  return subscriptions;
+};
+
+module.exports = {
+  querySubscriptions,
+};
