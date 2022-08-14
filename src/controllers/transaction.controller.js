33ee95adc7b73eecacca857c4a5af075baa6226e
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-15 00:09:53 +0100

    Aadd transaction controller method to verify transaction

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -7,6 +7,12 @@ const initializeTransaction = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(result);
 });
 
+const verifyTransaction = catchAsync(async (req, res) => {
+  const result = await transactionService.verifyTransaction(req.params.reference);
+  res.status(httpStatus.OK).send(result);
+});
+
 module.exports = {
   initializeTransaction,
+  verifyTransaction,
 };
