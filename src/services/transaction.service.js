5927eca98219b71bddac1e8d5a2f06521d1f8f36
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-14 23:51:12 +0100

    Update transaction service

--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -1,5 +1,6 @@
 const httpStatus = require('http-status');
 const paystack = require('paystack')(require('../config/config').paystack.secretKey);
+const { client } = require('../config/config');
 const ApiError = require('../utils/ApiError');
 const { db } = require('../models');
 
@@ -9,32 +10,34 @@ const { db } = require('../models');
  * @returns {Promise<Object>}
  */
 const initializeTransaction = async (transactionBody) => {
-  const subscriptionPlan = await db.subscriptionPlan.findOne({
+  const subscriptionPlan = await db.subscription_plans.findOne({
     where: { name: transactionBody.subscriptionPlanName },
   });
-  paystack.transaction.initialize(
+  if (!subscriptionPlan) throw new ApiError(httpStatus.NOT_FOUND, 'Subscription plan not found');
+  const result = await paystack.transaction.initialize(
     {
       email: transactionBody.email,
       amount: subscriptionPlan.price, // in kobo (100 kobo = 1 naira)
       currency: transactionBody.currency || 'NGN',
     },
-    function (error, body) {
+    async function (error, body) {
       if (error) {
         throw new ApiError(httpStatus.BAD_REQUEST, 'An error occurred');
       } else {
         const { data } = body;
-        db.transactions.create({
-          status: data.status,
+        await db.transactions.create({
+          status: body.status ? 'initiated' : 'failed',
           email: transactionBody.email,
-          authorizationUrl: data,
+          authorizationUrl: data.authorization_url,
           reference: data.reference,
           accessCode: data.access_code,
           amount: subscriptionPlan.price,
         });
-        return { authorizationUrl: body.data.authorization_url };
+        return { authorizationUrl: body.data.authorization_url, reference: body.data.reference };
       }
     }
   );
+  return result;
 };
 
 /**
@@ -50,8 +53,18 @@ const verifyTransaction = async (reference) => {
     if (transaction.status !== response.data.status) {
       transaction.update({ status: response.data.status });
     }
-    // Record the transaction a subscription
-    return { status: response.data.status };
+
+    // Record the transaction as a subscription
+
+    const registrationUrl = `${client.baseUrl}/register/${reference}`;
+
+    // Todo
+    // Send email to the transaction email with registration link
+
+    return {
+      status: response.data.status,
+      registrationUrl,
+    };
   } catch (error) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'An error occurred');
     // console.error(error);
@@ -68,9 +81,14 @@ const verifyTransaction = async (reference) => {
  * @returns {Promise<QueryResult>}
  */
 const queryTransactions = async (filter, options) => {
+  if (options.sortBy !== undefined) {
+    // eslint-disable-next-line no-param-reassign
+    options.order = [[options.sortBy, options.direction]];
+  }
+
   // Todo
   // Change options.sortBy of format: sortField:(desc|asc) to options.order of format: [['sortField', 'DESC'|'ASC']]
-  const transactions = await db.transactions.paginate({ ...options });
+  const transactions = await db.transactions.paginate({ where: filter, ...options });
   return transactions;
 };
 
@@ -82,7 +100,7 @@ const queryTransactions = async (filter, options) => {
 const getTransactionByReference = async (reference) => {
   const transaction = db.transactions.findOne({ where: { reference } });
 
-  if (!transaction) throw new ApiError(httpStatus.NOT_FOUND);
+  if (!transaction) throw new ApiError(httpStatus.NOT_FOUND, 'Transaction not found');
   return transaction;
 };
 
