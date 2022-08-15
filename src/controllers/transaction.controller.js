b2aa5b5091ed23659503d3bc921a132d158521aa
emmiiorji <emmanuelorji2013@gmail.com>
2022-08-16 00:14:19 +0100

    Add transaction controller method to get transaction by reference

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,5 +1,6 @@
 const httpStatus = require('http-status');
 const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { transactionService } = require('../services');
 
@@ -21,8 +22,17 @@ const getTransactions = catchAsync(async (req, res) => {
   res.send(transactions);
 });
 
+const getTransactionByReference = catchAsync(async (req, res) => {
+  const transaction = await transactionService.getTransactionByReference(req.params.reference);
+  if (!transaction) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Transaction not found');
+  }
+  res.send(transaction);
+});
+
 module.exports = {
   initializeTransaction,
   verifyTransaction,
   getTransactions,
+  getTransactionByReference,
 };
