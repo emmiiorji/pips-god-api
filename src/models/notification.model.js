47910183502c470c4958f2a1bcfa875ab5fa37d5
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-11 05:49:23 +0100

    Create nofication model

---
+++ src/models/notification.model.js
@@ -0,0 +1,25 @@
+const sequelizePaginate = require('sequelize-paginate');
+
+const { notificationStatusTypes } = require('../config/notificationStatuses');
+
+module.exports = (sequelize, dataType) => {
+  const notification = sequelize.define('notification', {
+    title: {
+      type: dataType.STRING(100),
+      trim: true,
+      allowNull: false,
+    },
+    body: {
+      type: dataType.TEXT,
+      trim: true,
+      allowNull: false,
+    },
+    status: {
+      type: dataType.ENUM(...Object.values(notificationStatusTypes)),
+      defaultValue: notificationStatusTypes.UNREAD,
+    },
+  });
+
+  sequelizePaginate.paginate(notification);
+  return notification;
+};
