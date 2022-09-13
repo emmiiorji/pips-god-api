d06999d1be34622ccf7856f5d74348e83a59b784
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-13 06:06:17 +0100

    Add method create dummy roles

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -23,7 +23,34 @@ const createDummySubscriptionPlans = async () => {
     logger.info('Created dummy subscription plans');
   }
 };
+const createDummyRoles = async () => {
+  const dummyRoles = [
+    {
+      name: 'admin',
+      description: 'An admin user',
+    },
+    {
+      name: 'user',
+      description: 'A normal user',
+    },
+    {
+      name: 'superuser',
+      description: 'Admin of admins',
+    },
+  ];
+
+  // Get existing plans
+  const allRoles = await db.roles.findAll();
+
+  // Create the plans in dummyPlans that don't exist
+  if (allRoles.length === 0 || allRoles.length !== dummyRoles.length) {
+    const filteredRoles = dummyRoles.filter((role) => !allRoles.find((r) => r.name === role.name));
+    await db.roles.bulkCreate(filteredRoles);
+    logger.info('Created dummy roles');
+  }
+};
 
 module.exports = {
   createDummySubscriptionPlans,
+  createDummyRoles,
 };
