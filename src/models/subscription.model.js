ed4371ebda13fd45259feefbb11104e2a64d7e66
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-19 06:15:55 +0100

    Add status to subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,7 +1,12 @@
 const sequelizePaginate = require('sequelize-paginate');
 
-module.exports = (sequelize) => {
-  const subscription = sequelize.define('subscription', {});
+module.exports = (sequelize, dataType) => {
+  const subscription = sequelize.define('subscription', {
+    isValid: {
+      type: dataType.BOOLEAN,
+      defaultValue: true,
+    },
+  });
 
   sequelizePaginate.paginate(subscription);
   return subscription;
