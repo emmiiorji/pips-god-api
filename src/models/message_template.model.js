fb0553a373b38aa07cafdb1f0c6b6ae37a091d87
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-21 17:42:46 +0100

    Create message_template model

---
+++ src/models/message_template.model.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, dataType) => {
+  const messageTemplate = sequelize.define('message_template', {
+    title: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    description: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    smsSubject: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    emailSubject: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    smsBody: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    emailBody: {
+      type: dataType.STRING,
+      allowNull: false,
+      trim: true,
+    },
+  });
+
+  return messageTemplate;
+};
