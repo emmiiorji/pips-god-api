47523cee50406c1adacaf6a654471db616664100
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-23 17:51:33 +0100

    Import message_template and variable validations within validation index

--- src/validations/index.js
+++ src/validations/index.js
@@ -2,3 +2,5 @@ module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
 module.exports.subscriptionValidation = require('./subscription.validation');
 module.exports.transactionValidation = require('./transaction.validation');
+module.exports.messageTemplateValidation = require('./message_template.validation');
+module.exports.messageTemplateVariableValidation = require('./variable.validation');
