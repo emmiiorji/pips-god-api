66f96999d00df5715ccd2b48b3576119b5d61293
emmiiorji <emmanuelorji2013@gmail.com>
2022-10-14 11:07:12 +0100

    Remove status message from reset password api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,8 +33,8 @@ const forgotPassword = catchAsync(async (req, res) => {
 });
 
 const resetPassword = catchAsync(async (req, res) => {
-  const message = await authService.resetPassword(req.query.token, req.body);
-  res.status(httpStatus.OK).send({ message });
+  await authService.resetPassword(req.query.token, req.body);
+  res.status(httpStatus.NO_CONTENT).send();
 });
 
 const sendVerificationEmail = catchAsync(async (req, res) => {
