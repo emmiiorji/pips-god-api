3b3213a5e9feed951d8ef5ce5f54789729485b65
emmiiorji <emmanuelorji2013@gmail.com>
2022-10-16 15:42:51 +0100

    Update method to send verification email in email service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -47,14 +47,21 @@ If you did not request any password reset, we assure that your account is secure
  * @param {string} token
  * @returns {Promise}
  */
-const sendVerificationEmail = async (to, token) => {
+const sendVerificationEmail = async (user, token, transactionId) => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `${config.client.baseUrl}/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
-  await sendEmail(to, subject, text);
+  const verificationEmailUrl = `${config.client.baseUrl}/verify-email?token=${token}${transactionId ? '&trans=' : ''}${
+    transactionId || ''
+  }`;
+  const text = `Dear ${user.firstName || 'user'},
+
+Welcome to Pipsgod Academy. To verify your email, click on this link: ${verificationEmailUrl}
+
+If you did not create an account, kindly ignore this email.
+
+Regards,
+The Pipsgod Team.`;
+  await sendEmail(user.email, subject, text);
 };
 
 module.exports = {
